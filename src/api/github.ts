ba12545a78c45f307bd4e140e8de7c41187afa2b
VictorNatan210 <victornatanalveshanusch@gmail.com>
2025-04-06 03:48:33 +0000

    refactor: altera a API para usar NextResponse e atualiza a lógica de busca de usuário

--- src/api/github.ts
+++ src/api/github.ts
@@ -1,18 +1,18 @@
-import { NextApiResponse, NextApiRequest } from 'next';
-import { Octokit } from "@octokit/rest";
+import { NextResponse } from 'next/server';
+import { Octokit } from '@octokit/rest';
 
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
-export default async function getGithubUser(req: NextApiRequest, res: NextApiResponse) {
-    const { username } = req.query;
-    if (typeof username !== 'string') {
-        return res.status(400).json({ error: 'Username invalido.' })
-    }
-
-    try {
-        const { data: userinfo } = await octokit.users.getByUsername({ username: username });
-        return res.status(200).json(userinfo);
-    } catch (err:any) {
-        return res.status(err.status || 500).json({ error: err.message });
-    }
+export async function GET(req: Request) {
+  const { searchParams } = new URL(req.url);
+  const username = searchParams.get('username');
+  if (!username) {
+    return NextResponse.json({ error: 'Username inválido' }, { status: 400 });
+  }
+  try {
+    const { data } = await octokit.users.getByUsername({ username });
+    return NextResponse.json(data);
+  } catch (err: any) {
+    return NextResponse.json({ error: err.message }, { status: err.status || 500 });
+  }
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,38 @@
+"use client"
+import { useState } from "react";
 import Image from "next/image";
-import { getUserInfo } from "../lib/getUserInfo";
 import styles from "./page.module.css";
-import { Octokit } from "octokit";
 
 export default function Home() {
-  function getUserByUsername() {}
+  const [input, setInput] = useState<string>('');
+  const [user, setUser] = useState<any>(null);
+ 
+  const request = async () => {
+    const res = await fetch(`/api/github?username=${input}`);
+    if(res.ok) {
+      const data = await res.json();
+      setUser(data);
+    } else {
+      console.error(await res.json());
+    }
+  }
 
   return (
     <div className={styles.page}>
       <main className={styles.main}>
         <section>
           <h1>Buscar informações</h1>
-          <form action="" method="get">
-            <label htmlFor="iuser">Nome de Usuário:</label>
-            <input type="text" name="user" id="iuser" />
+          <label htmlFor="iuser">Nome de Usuário:</label>
+          <input onChange={x => setInput(x.target.value)} type="text" name="user" id="iuser" />
 
-            <input type="submit" value="Enviar" />
-          </form>
+          <input type="submit" onClick={request} value="Enviar" />
+        </section>
+        <section>
+          {user && (
+            <div>
+              <h1>{user.name}</h1>
+            </div>
+          )}
         </section>
       </main>
       <footer className={styles.footer}></footer>
